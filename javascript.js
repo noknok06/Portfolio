857e1590e084bed41df5a44e9eb67259bd63e8a4
naoki0618 <nok.h0618@gmail.com>
2023-07-27 14:48:11 +0900

    add js

--- javascript.js
+++ javascript.js
@@ -20,4 +20,18 @@ $(function($) {
             $(this).css('color', 'white');
         }
     );
+
+    $(".timeline-header span").click(function() {
+        var index = $(".timeline-header span").index(this);
+        var $select = $(".timeline-content").eq(index);
+
+        var is_open = $select.find('.timeline-header span').text();
+        if (is_open == '+') {
+            $select.find('p').slideDown();
+            $select.find('.timeline-header span').text("-");
+        } else {
+            $select.find('p').slideUp();
+            $select.find('.timeline-header span').text("+");
+        }
+    });
 });
